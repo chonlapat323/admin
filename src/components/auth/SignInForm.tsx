210bc47195ecfb1ef41532a95311597b33e566ad
chonlapat323 <cschonlapat@gmail.com>
2025-03-22 22:12:53 +0700

    handle logout function & set api url config

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -7,7 +7,7 @@ import Button from "@/components/ui/button/Button";
 import { ChevronLeftIcon, EyeCloseIcon, EyeIcon } from "@/icons";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-
+import { API_URL } from "@/lib/config";
 export default function SignInForm() {
   const router = useRouter();
 
@@ -26,7 +26,7 @@ export default function SignInForm() {
     e.preventDefault();
     try {
       // เรียก API /api/login
-      const res = await fetch("http://localhost:3001/auth/login", {
+      const res = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,10 +1,9 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
 import React, { useState } from "react";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
-
+import { API_URL } from "@/lib/config";
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -16,6 +15,20 @@ function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
   function closeDropdown() {
     setIsOpen(false);
   }
+
+  const handleLogout = async () => {
+    try {
+      await fetch(`${API_URL}/auth/logout`, {
+        method: 'POST',
+        credentials: 'include',
+      });
+      window.location.href = '/signin';
+    } catch (err) {
+      console.error('Logout failed:', err);
+    }
+  };
+
+  
   return (
     <div className="relative">
       <button
@@ -144,8 +157,8 @@ function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
             </DropdownItem>
           </li>
         </ul>
-        <Link
-          href="/signin"
+        <button
+          onClick={handleLogout}
           className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
         >
           <svg
@@ -164,7 +177,7 @@ function toggleDropdown(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
             />
           </svg>
           Sign out
-        </Link>
+        </button>
       </Dropdown>
     </div>
   );
