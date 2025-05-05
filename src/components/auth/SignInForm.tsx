d46fa79c547d859ac4076abc6e6b94877a39eafb
chonlapat323 <cschonlapat@gmail.com>
2025-05-06 01:03:23 +0700

    fix login

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -141,7 +141,7 @@ export default function SignInForm() {
                   </span>
                 </div>
                 <Link
-                  href="/reset-password"
+                  href="#"
                   className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
                 >
                   Forgot password?
